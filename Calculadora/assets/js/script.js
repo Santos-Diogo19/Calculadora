3f4287d1ea9dec26c63e6f682334c4f629f6e14a
Diogo da Silva Santos <121092235+Santos-Diogo19@users.noreply.github.com>
2023-02-27 15:34:47 -0300

    Update script.js

--- Calculadora/assets/js/script.js
+++ Calculadora/assets/js/script.js
@@ -3,15 +3,15 @@ function criaCalculadora(){
         display: document.querySelector('.display'),
 
         inicia(){
-            this.cliqueBotoes();
-            this.pressionaEnter();
+            this.cliqueBotoes()
+            this.pressionaEnter()
             
         },
 
         pressionaEnter(){
             this.display.addEventListener('keyup', (e) =>{
                 if(e.keyCode === 13){
-                    this.realizaConta();
+                    this.realizaConta()
                 }
             })
         },
@@ -21,21 +21,21 @@ function criaCalculadora(){
         },
 
         realizaConta(){
-            let conta = this.display.value;
+            let conta = this.display.value
 
             try{
-                conta = eval(conta);
+                conta = eval(conta)
 
                 if(!conta){
-                    alert('Conta inválida');
-                    this.display.value = "";
+                    alert('Conta inválida')
+                    this.display.value = ""
                     return
                 }
                 this.display.value = String(conta)
             }catch(e){
-                alert('Conta inválida');
-                this.display.value = "";
-                return;
+                alert('Conta inválida')
+                this.display.value = ""
+                return
             }
             
         },
@@ -43,31 +43,31 @@ function criaCalculadora(){
 
         cliqueBotoes() {
             document.addEventListener('click', (e)=>{
-              const el = e.target;
+              const el = e.target
       
               if(el.classList.contains('btn-num')) {
-                this.btnParaDisplay(el.innerText);
+                this.btnParaDisplay(el.innerText)
               }
 
               if(el.classList.contains('btn-clear')){
-                this.display.value = "";
+                this.display.value = ""
               }
 
               if(el.classList.contains('btn-del')){
-                this.apagaUm();
+                this.apagaUm()
               }
              
               if(el.classList.contains('btn-eq')){
-                this.realizaConta();
+                this.realizaConta()
               }
             });
           },
 
         btnParaDisplay(valor) {
-            this.display.value += valor;
+            this.display.value += valor
           }
     };
 }
 
-const calculadora = criaCalculadora();
-calculadora.inicia();
+const calculadora = criaCalculadora()
+calculadora.inicia()
